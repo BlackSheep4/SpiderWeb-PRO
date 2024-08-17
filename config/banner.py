d42fd2d5fc9d19e7704d1d7a8c3d02c225d740e0
BlackSheep4 <alvaroblc@icloud.com>
2024-08-17 17:56:05 +0200

    Initial Commitment

--- config/banner.py
+++ config/banner.py
@@ -10,7 +10,7 @@ GITHUB = "https://github.com/BlackSheep4"
 version = 2.0
 
 def print_banner():
-    print(Fore.RED + f"""\n\n\t\t\tAIO Tool For """ + Fore.GREEN + f"""Information Gathering """ + Fore.RED + f"""And """ + Fore.GREEN + f"""Reconnaissance Scanner\n\t\t\t\t\t\t\t""" + Fore.RED + f"""[$] Current Version: """ +  Fore.GREEN + f"""{version}""")
+    print(Fore.RED + f"""\n\n\t\t\tAIO Tool For """ + Fore.GREEN + f"""Information Gathering """ + Fore.RED + f"""And """ + Fore.GREEN + f"""Reconnaissance Scanner\n\t\t\t\t\t""" + Fore.RED + f"""[$] Current Version: """ +  Fore.GREEN + f"""{version}""")
     print(Fore.RED + f"""
                                                                                                                  
   ______             __        __                      __       __            __                                        
@@ -27,6 +27,6 @@ def print_banner():
            \$$                                                                                                                                                                                                                            
     """)
 
-    print(f"""\n\t\t\t\t""" + Fore.GREEN + f"""\t\tCoded By - """ + Fore.RED + f"""{MAINTAINER}""" + Fore.GREEN + f"""\n\t\t\tFind me on GitHub: """ + Fore.RED + f"""{GITHUB}\n""" + Style.RESET_ALL)
+    print(f"""\n\t\t\t\t""" + Fore.GREEN + f"""\t\tCoded By - """ + Fore.RED + f"""{MAINTAINER}""" + Fore.GREEN + f"""\n\t\t\t\t\tFind me on GitHub: """ + Fore.RED + f"""{GITHUB}\n""" + Style.RESET_ALL)
 
 print_banner()
