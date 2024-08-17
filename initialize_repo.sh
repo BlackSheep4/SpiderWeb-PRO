8b0d5ec5bf180cead507319556cd8dd1989bf48b
BlackSheep4 <alvaroblc@icloud.com>
2024-08-17 15:46:34 +0200

    Initial Commitment

--- initialize_repo.sh
+++ initialize_repo.sh
@@ -4,7 +4,7 @@
 PROJECT_PATH="/Users/alvaro/Desktop/PROJECTS/SPIDERWEB/spider-web-v2"
 
 # Nombre del repositorio remoto en GitHub
-REPO_NAME="SpiderWeb-DEV"
+REPO_NAME="SpiderWeb-PRO"
 GITHUB_USER="BlackSheep4"
 REMOTE_URL="https://github.com/$GITHUB_USER/$REPO_NAME.git"
 
@@ -25,10 +25,10 @@ initialize_and_push() {
   git remote add origin $REMOTE_URL
 
   # Hacer pull de la rama remota para evitar conflictos
-  git pull origin master --allow-unrelated-histories
+  git pull origin dev --allow-unrelated-histories
 
   # Hacer push al repositorio remoto
-  git push -u origin master
+  git push -u origin dev
 }
 
 # Llamar a la función
