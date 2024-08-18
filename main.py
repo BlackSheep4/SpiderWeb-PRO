6fbba47fcb45db7256293415f79c78e033f0e73e
BlackSheep4 <alvaroblc@icloud.com>
2024-08-18 21:04:49 +0200

    Initial Commitment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import sys
+import argparse
 from config import banner
 from config.libraries import subprocess, Fore, Style, init
 from modules.advanced_os_detection import AdvancedStealthOSDetection
@@ -8,29 +8,35 @@ from modules.waf_detection import firewall_detection
 from modules.helper_panel import help_panel
 from modules.about_me import aboutme_info
 
-# Global Variables
-option = sys.argv[1]
-target = sys.argv[2]
+def main_function(args):
+    # Mostrar el banner
+    banner()
 
-def main_function():
-
-    banner
-
-    if option == "help":
+    if args.option == "help":
         help_panel()
-    
-    elif option == "info":
+
+    elif args.option == "info":
         aboutme_info()
 
-    elif option == "scan":
-        AdvancedStealthOSDetection(target)
-        AdvancedStealthOSDetection(target).passive_ttl_method()
-        AdvancedStealthOSDetection(target).active_nmap_method()
-        AdvancedStealthOSDetection(target).banner_grabbing()
-        AdvancedStealthOSDetection(target).pypof_detection()
+    elif args.option == "scan":
+        os_detection = AdvancedStealthOSDetection(args.target)
+        os_detection.passive_ttl_method()
+        os_detection.active_nmap_method()
+        os_detection.banner_grabbing()
+        os_detection.pypof_detection()
 
     else:
-        print(f"Specified command does not exist")
+        print(f"Specified command '{args.option}' does not exist.")
 
 if __name__ == "__main__":
-    main_function()
+
+    parser = argparse.ArgumentParser(description="AIO Tool For Information Gathering And Automatic Vulnerability Scanner")
+    parser.add_argument('option', choices=['help', 'info', 'scan'], help="Command to execute: 'help', 'info', or 'scan'.")
+    parser.add_argument('target', nargs='?', help="Target for the scan (required for 'scan' command).")
+
+    args = parser.parse_args()
+
+    if args.option == 'scan' and not args.target:
+        parser.error("The 'scan' command requires the target argument.")
+    
+    main_function(args)
