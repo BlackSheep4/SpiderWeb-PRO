7f4d8aad0e3adde6e4f174c204338565bc21f14a
BlackSheep4 <alvaroblc@icloud.com>
2024-08-18 16:14:58 +0200

    Initial Commitment

--- config/banner.py
+++ config/banner.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 from .libraries import Fore, Style, init
+import _curses
 
 # Const
 MAINTAINER = "BlackSheep4"
@@ -10,8 +11,8 @@ GITHUB = "https://github.com/BlackSheep4"
 version = 2.0
 
 def print_banner():
-    print(Fore.RED + f"""\n\n\t\t\tAIO Tool For """ + Fore.GREEN + f"""Information Gathering """ + Fore.RED + f"""And """ + Fore.GREEN + f"""Reconnaissance Scanner\n\t\t\t\t\t""" + Fore.RED + f"""[$] Current Version: """ +  Fore.GREEN + f"""{version}""")
-    print(Fore.RED + f"""
+    print(Fore.RED + r"""\n\n\t\t\tAIO Tool For """ + Fore.GREEN + r"""Information Gathering """ + Fore.RED + r"""And """ + Fore.GREEN + r"""Reconnaissance Scanner\n\t\t\t\t\t""" + Fore.RED + r"""[$] Current Version: """ +  Fore.GREEN + f"""{version}""")
+    print(Fore.RED + r"""
                                                                                                                  
   ______             __        __                      __       __            __                                        
  /      \           |  \      |  \                    |  \  _  |  \          |  \      
@@ -29,4 +30,18 @@ def print_banner():
 
     print(f"""\n\t\t\t\t""" + Fore.GREEN + f"""\tCoded By - """ + Fore.RED + f"""{MAINTAINER}""" + Fore.GREEN + f"""\n\tFind me on GitHub: """ + Fore.RED + f"""{GITHUB}\n""" + Style.RESET_ALL)
 
+try:
+    init()
+except _curses.error:
+    print("Terminal features are unavailable. Continuing without them.")
+
 print_banner()
+
+def main_function():
+    try:
+        target = input("\n[#] Enter the website to scan (e.g: example.com) >> ")
+    except EOFError:
+        print("No input received. Exiting...")
+        exit(1)
+
+main_function()
